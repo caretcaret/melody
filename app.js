4325407ac0275d5338d8015e55b0dbc1ec08e3ed
Jeffrey Zhang <caretcaret@gmail.com>
2014-01-03 13:46:53 -0800

    More express/node setup

--- app.js
+++ app.js
@@ -1,8 +1,73 @@
-var express = require('express');
+var express = require('express'),
+  slash = require('express-slash'),
+  exphbs = require('express3-handlebars'),
+  mongoose = require('mongoose'),
+  MongoStore = require('connect-mongo')(express),
+  env = process.env.NODE_ENV || 'development',
+  config = require('./config/config')[env];
+
+// create and configure express app
 var app = express();
 
+// use handlebar templates with extension .html
+var hbs = exphbs.create({
+  defaultLayout: 'main',
+  extname: '.html',
+  helpers: {}
+});
+app.engine('html', hbs.engine);
+app.set('view engine', 'html');
+
+app.set('port', config.port);
+app.set('title', config.app.name);
+
+app.use(express.logger('dev'));
+app.use(express.compress());
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(express.methodOverride());
+
+app.use(express.cookieParser());
+app.use(express.session({
+  secret: config.secret,
+  maxAge: new Date(Date.now() + 1000 * 60 * 60 * 24 * 7),
+  store: new MongoStore(config.db)
+}));
+
+app.enable('strict routing');
+app.use(app.router);
+app.use(slash());
+app.use(express.static(config.root + '/public'));
+
+if (env === 'development' || env === 'test') {
+  app.use(express.errorHandler({
+    dumpExceptions: true,
+    showStack: true
+  }));
+} else if (env === 'production') {
+  app.use(express.errorHandler());
+}
+
 app.get('/', function(req, res) {
-	res.send('hello world');
+  res.render('home', {text: 'hello world'});
 });
 
-app.listen(3000);
+
+function dburl(dbobj) {
+  return 'mongodb://' + dbobj.host + '/' + dbobj.db;
+}
+
+mongoose.connect(dburl(config.db));
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+
+db.once('open', function() {
+  console.log("Database connection open");
+  var server = require('http').createServer(app);
+  var io = require('socket.io').listen(server);
+  server.listen(app.get('port'));
+  console.log("Web server listening on port " + app.get('port'));
+});
+
+
+exports = module.exports = app;
---
+++ config/config.js
@@ -0,0 +1,43 @@
+var _ = require('underscore'),
+  path = require('path');
+
+global = {
+  root: path.normalize(__dirname + '../'),
+  app: {
+    name: 'Melody'
+  }
+};
+
+development = {
+  db: {
+    db: 'melody-dev',
+    host: 'localhost'
+  },
+  port: 3000,
+  secret: 'development',
+};
+
+test = {
+  db: {
+    db: 'melody-test',
+    host: 'localhost'
+  },
+  port: 3000,
+  secret: 'testing'
+};
+
+production = {
+  db: {
+    db: 'melody',
+    host: 'localhost'
+  },
+  port: 80,
+  secret: 'production' // change to actual secret
+};
+
+
+module.exports = {
+  development: _.extend({}, global, development),
+  test: _.extend({}, global, test),
+  production: _.extend({}, global, production)
+};
