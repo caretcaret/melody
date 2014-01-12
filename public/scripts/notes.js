f45d00c8813db6da9f3f3388f6c06be19724b028
Jeffrey Zhang <caretcaret@gmail.com>
2014-01-12 18:44:58 -0500

    Tiled layout for note previews

--- public/scripts/notes.js
+++ public/scripts/notes.js
@@ -1,4 +1,4 @@
-var hideNote = function(){
+/*var hideNote = function(){
   socket.emit('hideNote', $(this).attr('id'));
 };
 
@@ -41,6 +41,34 @@ device.onPaste(function(value) {
     // upload the file
     console.log(value.data);
     ss(socket).emit('createImage', stream, {size: value.data.size, type: value.data.type});
+    ss.createBlobReadStream(value.data).pipe(stream);
+  }
+});
+*/
+
+var acceptPastes = true;
+
+device.initPaste();
+device.onPaste(function(value) {
+  console.log(value.type + " paste received.");
+
+  if (!acceptPastes) {
+    console.log("Paste rejected.");
+    return;
+  }
+
+  if (value.type === 'text' || value.type === 'html') {
+    // don't use a stream, just a regular text
+    socket.emit('createText', value);
+  } else if (value.type === 'image') {
+    var stream = ss.createStream();
+
+    // upload the file
+    ss(socket).emit('createImage', stream,
+      {
+        size: value.data.size,
+        type: value.data.type
+      });
     ss.createBlobReadStream(value.data).pipe(stream);
   }
 });
